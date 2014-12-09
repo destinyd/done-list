9ea099df13898bfb9b7fff42852b72518ffe2591
destinyd <destinyd_war@163.com>
2014-12-09 02:56:01 +0000

    dashboard base

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,11 @@ class HomeController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
+  def dashboard
+    @tasks_count = current_user.tasks.count
+    @recent_tasks_count = current_user.tasks.where(:finished_at.gt => 30.days.ago).count
+
+    @targets = Target.important.limit(5)
+  end
 end
