3593ec29c7f8239173c7073ada8724a19733338a
destinyd <destinyd_war@163.com>
2014-12-17 11:08:51 +0800

    star targets hash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,8 +13,15 @@ class HomeController < ApplicationController
     @tasks_count = current_user.tasks.count
     @recent_tasks = current_user.tasks.five_day
     @recent_tasks_count = @recent_tasks.count
-    @targets = current_user.targets.important.limit(5).to_a
     @date_tasks_hash = @recent_tasks.group_by{|t| t.finished_at.strftime("%Y-%m-%d")}
     @color_num = 3312336
+    max_count = current_user.targets.important.first.tasks_count
+    min_count = current_user.targets.important.last.tasks_count
+    max_count -= 0.001 if max_count > min_count
+    delta = (max_count - min_count) / 5.0
+    @star_targets_hash = current_user.targets.important.group_by do |target|
+      v = (target.tasks_count / delta).floor
+      v > 4 ? 4 : v
+    end
   end
 end
